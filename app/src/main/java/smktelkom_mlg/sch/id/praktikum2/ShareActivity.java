981bd4b67382a240a6ee8d369b5f937a3a46714f
Ain <nurul_ain_26rpl@student.smktelkom-mlg.sch.id>
2019-04-12 08:43:47 +0700

    selesai

---
+++ app/src/main/java/smktelkom_mlg/sch/id/praktikum2/ShareActivity.java
@@ -0,0 +1,13 @@
+package smktelkom_mlg.sch.id.praktikum2;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class ShareActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_share);
+    }
+}
